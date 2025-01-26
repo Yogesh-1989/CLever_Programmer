ba27239b3338f7b9a9c575ae33552f1575c7a56c
Yogesh Bhamare <Yogi_Bhamare@gmail.com>
2025-01-27 01:14:11 +0530

    few changes in code

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,5 +3,7 @@ tip_percentage = int(input("Enter percentage of tip:"))
 
 tip = (tip_percentage/100) * food_bill
 
-print("Therefore tip amount to is: ", round(tip, 1))
+print("Therefore tip amount to is: ", round(tip))
+
+print("Total bill at the end is:", tip + food_bill,"Rupees")
           
